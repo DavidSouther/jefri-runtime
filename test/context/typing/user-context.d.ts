4abc89db93a3a8e49a868ea3e988d61bfde8a25c
David Souther <davidsouther@gmail.com>
2015-11-24 15:35:23 -0500

    Using generated .d.ts

---
+++ test/context/typing/user-context.d.ts
@@ -0,0 +1,28 @@
+declare module UserContext {
+  interface Context {
+    User: UserContext.User;
+    Authinfo: UserContext.Authinfo;
+  }
+
+  interface User extends JEFRi.Entity {
+    user_id: string;
+    name: string;
+    address: string;
+    nicknames: string[];
+    authinfo: Authinfo;
+  }
+
+  interface Authinfo extends JEFRi.Entity {
+    authinfo_id: string;
+    user_id: string;
+    username: string;
+    password: string;
+    user: User;
+  }
+}
+
+declare module "user-context" {
+  var c: UserContext.Context;
+  export = c;
+}
+
--- test/spec/smoke.ts
+++ test/spec/smoke.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import { Runtime, Entity } from '../../';
 
-interface User extends Entity {}
+import * as UserContext from 'user-context';
 
 describe('Smoke', function() {
   it('loads', function(done) {
     let runtime = new Runtime("https://raw.githubusercontent.com/jefri/runtime/master/test/context/context.json");
     runtime.ready.then(function() {
-      let user = runtime.build<User>('User');
+      let user = runtime.build<UserContext.User>('User');
       expect(user.id()).to.exist;
     }).then(done, done)
   });
