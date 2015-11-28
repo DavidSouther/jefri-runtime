0e79a9a1a5e8f3939ec6316c486d2ac0c048fb31
David Souther <davidsouther@gmail.com>
2015-11-27 20:39:53 -0500

    Mocha runs all tests.

--- test/spec/definitions.ts
+++ test/spec/definitions.ts
@@ -3,13 +3,13 @@ import { Runtime } from '../../';
 
 import * as UserContext from 'user-context';
 
-describe("Properties", function() {
+describe("Definition", function() {
   let runtime: Runtime = null;
   beforeEach(function(done) {
     runtime = new Runtime("https://raw.githubusercontent.com/jefri/runtime/master/test/context/user.json");
     runtime.ready.then(()=>done(), done);
   });
-  it("have good defaults", function() {
+  it.only("is available from an instance", function() {
     let user = runtime.build<UserContext.User>("User", {
       name: "southerd",
       address: "davidsouther@gmail.com"
